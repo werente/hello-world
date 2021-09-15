34907a187a80ea821f9ba457428e1286f269cba9
Jose Andres Rocha Rodriguez <ac19jrocha@inslaferreria.cat>
2021-09-15 15:09:39 +0200

    se ha hecho cambios

--- main.ts
+++ main.ts
@@ -1,22 +1,21 @@
- function emotion(): number {
-     let cad: string = "";
-     let st: number = 0;
-    if (input.pinIsPressed(TouchPin.P0)) {
-        cad = "TRANQUILIDAD";
-        basic.showString(cad)
-        st = 1;
-    }
-     if (input.pinIsPressed(TouchPin.P1)) {
-        cad = "AMOR";
-        basic.showString(cad)
-        st = 2
-    }
-    if (input.pinIsPressed(TouchPin.P2)) {
-        cad = "FELICIDAD";
-        basic.showString(cad) 
-        st = 3    
-    }
-    basic.clearScreen()
-     return st
-
+function emotion(): number {
+  let cad: string = '';
+  let st: number = 0;
+  if (input.pinIsPressed(TouchPin.P0)) {
+    cad = 'TRANQUILIDAD';
+    basic.showString(cad);
+    st = 1;
+  }
+  if (input.pinIsPressed(TouchPin.P1)) {
+    cad = 'AMOR';
+    basic.showString(cad);
+    st = 2;
+  }
+  if (input.pinIsPressed(TouchPin.P2)) {
+    cad = 'FELICIDAD';
+    basic.showString(cad);
+    st = 3;
+  }
+  basic.clearScreen();
+  return st;
 }
--- method.ts
+++ method.ts
@@ -1,22 +1,40 @@
-function method (): void{
-    let choice: number = Math.floor(Math.random()*3)+1
-    switch(emotion()){
-        case 1: {
-            if (choice = 1) { basic.showString('tranquila 1')}
-            if (choice = 2) { basic.showString('tranquila 2') }
-            if (choice = 3) { basic.showString('tranquila 3') }
-            break;
-        }
-        case 2: {
-            if (choice = 1) { basic.showString('amor 1') }
-            if (choice = 2) { basic.showString('amor 2') }
-            if (choice = 3) { basic.showString('amor 3') }
-            break;
-        }
-        case 3: {
-            if (choice = 1) { basic.showString('feliz 1') }
-            if (choice = 2) { basic.showString('feliz 2') }
-            if (choice = 3) { basic.showString(' feliz 3') }
-        }
+function method(): void {
+  let choice: number = Math.floor(Math.random() * 3) + 1;
+  switch (emotion()) {
+    case 1: {
+      if ((choice = 1)) {
+        basic.showString('calma');
+      }
+      if ((choice = 2)) {
+        basic.showString('sosiego');
+      }
+      if ((choice = 3)) {
+        basic.showString('t');
+      }
+      break;
     }
+    case 2: {
+      if ((choice = 1)) {
+        basic.showString('gusto');
+      }
+      if ((choice = 2)) {
+        basic.showString('querida');
+      }
+      if ((choice = 3)) {
+        basic.showString('a');
+      }
+      break;
+    }
+    case 3: {
+      if ((choice = 1)) {
+        basic.showString('happy');
+      }
+      if ((choice = 2)) {
+        basic.showString('felicitat');
+      }
+      if ((choice = 3)) {
+        basic.showString(' feliz');
+      }
+    }
+  }
 }
